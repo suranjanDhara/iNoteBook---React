5494a55932d2a0a42eb01e921a83f8e44949f7f0
suranjanDhara <108186929+suranjanDhara@users.noreply.github.com>
2022-12-27 07:39:40 +0530

    Backend server with express js and mongoose started

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const connectToMongo = require('./db')
+const express = require('express')
+
+
+const app = express()
+const port = 3000
+
+// Available routes
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
+
+connectToMongo()
